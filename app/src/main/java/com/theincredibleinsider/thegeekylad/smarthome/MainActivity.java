c3b157629f01e67ca77564771312dec46e356a5e
theGeekyLad <rahul.pillai03@gmail.com>
2017-10-10 02:40:51 +0530

    Initial commit.

---
+++ app/src/main/java/com/theincredibleinsider/thegeekylad/smarthome/MainActivity.java
@@ -0,0 +1,17 @@
+package com.theincredibleinsider.thegeekylad.smarthome;
+
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        float[] values;
+
+    }
+}
